b06d471984c2441792a978176b797203e718b3b8
moondev03 <moondev03@gmail.com>
2026-06-08 20:28:23 +0900

    feat: SaionFeatureImplPlugin에 UI 및 디자인 시스템 의존성 추가

--- Saion/build-logic/convention/src/main/java/com/saion/convention/SaionFeatureImplPlugin.kt
+++ Saion/build-logic/convention/src/main/java/com/saion/convention/SaionFeatureImplPlugin.kt
@@ -1,7 +1,10 @@
 package com.saion.convention
 
+import com.saion.convention.internal.library
+import com.saion.convention.internal.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class SaionFeatureImplPlugin : Plugin<Project> {
     override fun apply(target: Project) =
@@ -9,5 +12,10 @@ class SaionFeatureImplPlugin : Plugin<Project> {
             pluginManager.apply("com.saion.android.library")
             pluginManager.apply("com.saion.android.compose")
             pluginManager.apply("com.saion.android.testing")
+
+            dependencies {
+                add("implementation", project(":core:ui"))
+                add("implementation", libs.library("saion-design-system"))
+            }
         }
 }
