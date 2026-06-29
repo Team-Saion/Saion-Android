5111c5bae8515e6ae783df8001ff93d8b9c42674
moondev03 <moondev03@gmail.com>
2026-06-29 22:51:44 +0900

    feat: design-system 모듈에 Pretendard 폰트 추가

--- Saion/core/design-system/src/main/java/com/saion/ds/token/typography/FontFamily.kt
+++ Saion/core/design-system/src/main/java/com/saion/ds/token/typography/FontFamily.kt
@@ -1,5 +1,37 @@
 package com.saion.ds.token.typography
 
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.saion.core.designsystem.R
 
-internal val Pretendard: FontFamily = FontFamily.SansSerif
+internal val Pretendard: FontFamily = FontFamily(
+    Font(
+        resId = R.font.pretendard_thin,
+        weight = FontWeight.Thin,
+    ),
+    Font(
+        resId = R.font.pretendard_extra_light,
+        weight = FontWeight.ExtraLight,
+    ),
+    Font(
+        resId = R.font.pretendard_light,
+        weight = FontWeight.Light,
+    ),
+    Font(
+        resId = R.font.pretendard_regular,
+        weight = FontWeight.Normal,
+    ),
+    Font(
+        resId = R.font.pretendard_medium,
+        weight = FontWeight.Medium,
+    ),
+    Font(
+        resId = R.font.pretendard_semi_bold,
+        weight = FontWeight.SemiBold,
+    ),
+    Font(
+        resId = R.font.pretendard_bold,
+        weight = FontWeight.Bold,
+    ),
+)
