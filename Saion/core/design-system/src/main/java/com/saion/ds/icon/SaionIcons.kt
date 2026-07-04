1effd7ec18a1cb421635b07cc0ad4305a708ef3c
moondev03 <moondev03@gmail.com>
2026-07-04 18:04:00 +0900

    feat: CloseFilled 아이콘 추가

--- Saion/core/design-system/src/main/java/com/saion/ds/icon/SaionIcons.kt
+++ Saion/core/design-system/src/main/java/com/saion/ds/icon/SaionIcons.kt
@@ -1,6 +1,7 @@
 package com.saion.ds.icon
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.saion.ds.icon.vector.IcCloseFilled
 import com.saion.ds.icon.vector.IcChevronLeft
 import com.saion.ds.icon.vector.IcChevronRight
 import com.saion.ds.icon.vector.IcSelection
@@ -9,4 +10,5 @@ object SaionIcons {
     val Selection: ImageVector = IcSelection
     val ChevronRight: ImageVector = IcChevronRight
     val ChevronLeft: ImageVector = IcChevronLeft
+    val CloseFilled: ImageVector = IcCloseFilled
 }
---
+++ Saion/core/design-system/src/main/java/com/saion/ds/icon/vector/IcCloseFilled.kt
@@ -0,0 +1,79 @@
+package com.saion.ds.icon.vector
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val IcCloseFilled: ImageVector
+    get() {
+        if (_CloseFilled != null) {
+            return _CloseFilled!!
+        }
+        _CloseFilled = ImageVector.Builder(
+            name = "StyleFill",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f,
+        ).apply {
+            path(fill = SolidColor(Color(0xFF6C757F))) {
+                moveTo(12f, 2.25f)
+                curveTo(10.072f, 2.25f, 8.187f, 2.822f, 6.583f, 3.893f)
+                curveTo(4.98f, 4.965f, 3.73f, 6.487f, 2.992f, 8.269f)
+                curveTo(2.254f, 10.05f, 2.061f, 12.011f, 2.437f, 13.902f)
+                curveTo(2.814f, 15.793f, 3.742f, 17.531f, 5.106f, 18.894f)
+                curveTo(6.469f, 20.258f, 8.207f, 21.187f, 10.098f, 21.563f)
+                curveTo(11.989f, 21.939f, 13.95f, 21.746f, 15.731f, 21.008f)
+                curveTo(17.513f, 20.27f, 19.035f, 19.02f, 20.107f, 17.417f)
+                curveTo(21.178f, 15.813f, 21.75f, 13.928f, 21.75f, 12f)
+                curveTo(21.747f, 9.415f, 20.719f, 6.937f, 18.891f, 5.109f)
+                curveTo(17.063f, 3.281f, 14.585f, 2.253f, 12f, 2.25f)
+                close()
+                moveTo(15.531f, 14.469f)
+                curveTo(15.6f, 14.539f, 15.656f, 14.622f, 15.693f, 14.713f)
+                curveTo(15.731f, 14.804f, 15.75f, 14.901f, 15.75f, 15f)
+                curveTo(15.75f, 15.099f, 15.731f, 15.196f, 15.693f, 15.287f)
+                curveTo(15.656f, 15.378f, 15.6f, 15.461f, 15.531f, 15.531f)
+                curveTo(15.461f, 15.6f, 15.378f, 15.656f, 15.287f, 15.693f)
+                curveTo(15.196f, 15.731f, 15.099f, 15.75f, 15f, 15.75f)
+                curveTo(14.901f, 15.75f, 14.804f, 15.731f, 14.713f, 15.693f)
+                curveTo(14.622f, 15.656f, 14.539f, 15.6f, 14.469f, 15.531f)
+                lineTo(12f, 13.06f)
+                lineTo(9.531f, 15.531f)
+                curveTo(9.461f, 15.6f, 9.378f, 15.656f, 9.287f, 15.693f)
+                curveTo(9.196f, 15.731f, 9.099f, 15.75f, 9f, 15.75f)
+                curveTo(8.901f, 15.75f, 8.804f, 15.731f, 8.713f, 15.693f)
+                curveTo(8.622f, 15.656f, 8.539f, 15.6f, 8.469f, 15.531f)
+                curveTo(8.4f, 15.461f, 8.344f, 15.378f, 8.307f, 15.287f)
+                curveTo(8.269f, 15.196f, 8.25f, 15.099f, 8.25f, 15f)
+                curveTo(8.25f, 14.901f, 8.269f, 14.804f, 8.307f, 14.713f)
+                curveTo(8.344f, 14.622f, 8.4f, 14.539f, 8.469f, 14.469f)
+                lineTo(10.94f, 12f)
+                lineTo(8.469f, 9.531f)
+                curveTo(8.329f, 9.39f, 8.25f, 9.199f, 8.25f, 9f)
+                curveTo(8.25f, 8.801f, 8.329f, 8.61f, 8.469f, 8.469f)
+                curveTo(8.61f, 8.329f, 8.801f, 8.25f, 9f, 8.25f)
+                curveTo(9.199f, 8.25f, 9.39f, 8.329f, 9.531f, 8.469f)
+                lineTo(12f, 10.94f)
+                lineTo(14.469f, 8.469f)
+                curveTo(14.539f, 8.4f, 14.622f, 8.344f, 14.713f, 8.307f)
+                curveTo(14.804f, 8.269f, 14.901f, 8.25f, 15f, 8.25f)
+                curveTo(15.099f, 8.25f, 15.196f, 8.269f, 15.287f, 8.307f)
+                curveTo(15.378f, 8.344f, 15.461f, 8.4f, 15.531f, 8.469f)
+                curveTo(15.6f, 8.539f, 15.656f, 8.622f, 15.693f, 8.713f)
+                curveTo(15.731f, 8.804f, 15.75f, 8.901f, 15.75f, 9f)
+                curveTo(15.75f, 9.099f, 15.731f, 9.196f, 15.693f, 9.287f)
+                curveTo(15.656f, 9.378f, 15.6f, 9.461f, 15.531f, 9.531f)
+                lineTo(13.06f, 12f)
+                lineTo(15.531f, 14.469f)
+                close()
+            }
+        }.build()
+
+        return _CloseFilled!!
+    }
+
+@Suppress("ObjectPropertyName")
+private var _CloseFilled: ImageVector? = null
